11aefddcffd5921bd9f4954bc5cbfb8d99feaaaf
Jeremy <jeremy.manghisi@gmail.com>
2025-12-03 23:42:07 +0100

    Creamos proyecto Maven Fix #1

---
+++ gitflowjava/src/main/java/es/gva/edu/sanchezgarcia/jg/mavenproject54/App.java
@@ -0,0 +1,13 @@
+package es.gva.edu.sanchezgarcia.jg.mavenproject54;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
